50e541f4e36c1a5db20a15d78bb0bd1631ea56d0
Jose Luis <joseluismartinez1024@gmail.com>
2021-12-28 11:17:55 -0600

    Programar botón enviar.

--- Orden Compra/Form1.cs
+++ Orden Compra/Form1.cs
@@ -29,7 +29,64 @@ namespace Orden_Compra
 
         private void btnEnviar_Click(object sender, EventArgs e)
         {
+            if (txtName.Text.Length > 0)
+            {
+                frmResumen mandar = new frmResumen();
+                contador += 1;
+                mandar.lblPedido.Text = "No. de pedido: " + contador.ToString();
+                mandar.lblName.Text = "Nombre del cliente: " + txtName.Text;
+                mandar.lblHamb.Text = "Producto: Hamburguesa";
+                mandar.lblHot.Text = "Producto: Hotdog";
+                if (rbHaG.Checked == true)
+                {
+                    mandar.lblSizeHam.Text = "Tamaño: " + rbHaG.Text;
+                }
+                else
+                {
+                    mandar.lblSizeHam.Text = "Tamaño: " + rbHaCH.Text;
+                }
+                if (rbHoG.Checked == true)
+                {
+                    mandar.lblSizeHot.Text = "Tamaño: " + rbHoG.Text;
+                }
+                else
+                {
+                    mandar.lblSizeHot.Text = "Tamaño: " + rbHoC.Text;
+                }
 
+                if (chkIngHa1.Checked == true && chkIngHa2.Checked == true && chkIngHa3.Checked == true)
+                {
+                    mandar.lblIngHam.Text = "Ingredientes: " + chkIngHa1.Text + " " + chkIngHa2.Text + " " + chkIngHa3.Text;
+                }
+                else if (chkIngHa1.Checked == true && chkIngHa2.Checked == true)
+                {
+                    mandar.lblIngHam.Text = "Ingredientes: " + chkIngHa1.Text + " " + chkIngHa2.Text;
+                }
+                else if (chkIngHa1.Checked == true)
+                {
+                    mandar.lblIngHam.Text = "Ingredientes: " + chkIngHa1.Text;
+                }
+
+                if (chkIngHo1.Checked == true && chkIngHo2.Checked == true && chkIngHo3.Checked == true)
+                {
+                    mandar.lblIngHot.Text = "Ingredientes: " + chkIngHo1.Text + " " + chkIngHo2.Text + " " + chkIngHo3.Text;
+                }
+                else if (chkIngHo1.Checked == true && chkIngHo2.Checked == true)
+                {
+                    mandar.lblIngHot.Text = "Ingredientes: " + chkIngHo1.Text + " " + chkIngHo2.Text;
+                }
+                else if (chkIngHo1.Checked == true)
+                {
+                    mandar.lblIngHot.Text = "Ingredientes: " + chkIngHo1.Text;
+                }
+
+                mandar.lblAderezos.Text = "Aderezos: " + cmbAderezo.SelectedItem.ToString();
+                mandar.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Error en el envío.\nNo hay cliente registrado.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
